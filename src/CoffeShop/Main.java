dbb541ca07da652a37ac68864e08e1791b30f22a
Ertugrul Efe Kaya <efekaya2334@gmail.com>
2021-05-05 17:21:48 +0300

    homeworkDay4.2

---
+++ src/CoffeShop/Main.java
@@ -0,0 +1,20 @@
+package CoffeShop;
+
+import java.time.LocalDate;
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.NeroCustomerManager;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+		customerManager.save(new Customer(1, "Engin", "Demiroð", LocalDate.of(1985, 1, 6), "28861499108"));
+		customerManager.save(new Customer(2, "Ertuðrul Efe", "Kaya", LocalDate.of(2001, 2, 21), "29035547724"));
+		BaseCustomerManager customerManager2 = new NeroCustomerManager();
+		customerManager2.save(new Customer(2, "Ertuðrul Efe", "Kaya", LocalDate.of(2001, 2, 21), "29035547724"));
+	}
+
+}
